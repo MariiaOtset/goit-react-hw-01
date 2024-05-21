39c8c3afecfec2733733dd1a1a55998ab5f77587
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-05-21 16:24:16 +0100

    continue task 2

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -3,11 +3,10 @@ import css from "./FriendListItem.module.css"
 export const FriendListItem = ({ avatar, name, isOnline }) => {
     return (
     <>
-<div>
-  <img src={avatar} alt="Avatar" width="48" />
-  <p>{name}</p>
-                <p className={isOnline ? css.online : css.offline}>{isOnline ? 'Online' : 'Offline'}
-                </p>
+<div className={css.friendWrapper}>
+  <img className={css.friendImg} src={avatar} alt="Avatar" />
+  <p className={css.friendName}>{name}</p>
+    <p className={isOnline ? css.online : css.offline}>{isOnline ? 'Online' : 'Offline'}</p>
  
 </div>
     </>
