25ca007eb4bd8e0889b88faebb87d0a2f57bcb76
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-05-27 15:35:33 +0100

    styles for task 1 and 2 done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,36 +1,31 @@
 import css from "./Profile.module.css";
 
-
 export const Profile = ({ name, tag, location, image, stats }) => {
-    return (
-        <div className={css.profileCard}>
-<div>
- <img className={css.profileImg}
-   src={image}
-            alt="User avatar"
- />
- <p className={css.username}>{name}</p>
- <p className={css.text}>@{tag}</p>
- <p>{location}</p>
- </div>
+  return (
+    <div className={css.profileCard}>
+      <div>
+        <img className={css.profileImg} src={image} alt="User avatar" />
+        <p className={css.username}>{name}</p>
+        <p className={css.text}>@{tag}</p>
+        <p className={css.text}>{location}</p>
+      </div>
 
- <ul>
- <li>
- <span>Followers</span>
- <span>{stats.followers}</span>
- </li>
- <li>
- <span>Views</span>
- <span>{stats.views}</span>
- </li>
- <li>
- <span>Likes</span>
- <span>{stats.likes}</span>
- </li>
- </ul>
-</div>
+      <ul className={css.statsWrapper}>
+        <li className={css.stats}>
+          <span>Followers</span>
+          <span className={css.span}>{stats.followers}</span>
+        </li>
+        <li className={css.stats}>
+          <span>Views</span>
+          <span className={css.span}>{stats.views}</span>
+        </li>
+        <li className={css.stats}>
+          <span>Likes</span>
+          <span className={css.span}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-    )
-}
-
-export default Profile
+export default Profile;
